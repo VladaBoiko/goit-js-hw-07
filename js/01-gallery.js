616c06571a0b7822ea1d775814e2160a35e5cd88
VladaBoiko <bermezha@gmail.com>
2022-07-31 16:43:54 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,7 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+// 1. получаем масив обьектов, перебираем его на обьекты
+// 2. каждый обьект деструктуризируем
+// 3. создаем елементы изображения, пишем им в свойства ссылку

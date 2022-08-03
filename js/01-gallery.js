3d025f05cf0ff336d6c18c36773318beea5a6466
VladaBoiko <bermezha@gmail.com>
2022-08-03 16:45:25 +0300

    add homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,51 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const imgCards = addImgGallery(galleryItems);
 
-console.log(galleryItems);
-// 1. получаем масив обьектов, перебираем его на обьекты
-// 2. каждый обьект деструктуризируем
-// 3. создаем елементы изображения, пишем им в свойства ссылку
+galleryContainer.insertAdjacentHTML("beforeend", imgCards);
+galleryContainer.addEventListener("click", onGalleryContainer);
+
+function addImgGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function onGalleryContainer(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const modal = basicLightbox.create(
+    `<img src="${
+      event.target.closest("img").dataset.source
+    }" width="800" height="600">`
+  );
+
+  console.log(event.target);
+  console.log(modal.show());
+}
+
+document.addEventListener("keydown", (evt) => {
+  const pushEscape = evt.code === "Escape";
+  const onModal = document.querySelector(".basicLightbox");
+  if (!onModal) {
+    return;
+  }
+  if (pushEscape) {
+    onModal.remove();
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const imgCards = addImgGallery(galleryItems);
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", imgCards);
+galleryContainer.addEventListener("click", onGalleryContainer);
+
+function addImgGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+    </a>`;
+    })
+    .join("");
+}
+function onGalleryContainer(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  let gallery = new SimpleLightbox(".gallery a");
+
+  gallery.on("show.simplelightbox", function () {
+    gallery.defaultOptions.captionDelay = 250;
+  });
+}
